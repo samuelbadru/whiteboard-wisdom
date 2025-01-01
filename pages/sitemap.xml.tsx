8aaaed407f51366f7663427825d1d89654b0d2a1
Samuel Badru <samuelbadru@outlook.com>
2025-01-01 15:21:01 +0000

    Sitemap added

---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,46 @@
+import { getAllPostIds } from "../lib/posts";
+
+//pages/sitemap.xml.js
+const DOMAIN = "https://mywisdomboard.com";
+
+function generateSiteMap(posts) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>${DOMAIN}</loc>
+      </url>
+     ${posts
+       .map(({ params: { id } }) => {
+         return `
+       <url>
+           <loc>${`${DOMAIN}/posts/${id}`}</loc>
+       </url>
+     `;
+       })
+       .join("")}
+   </urlset>
+ `;
+}
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export async function getServerSideProps({ res }) {
+  // We make an API call to gather the URLs for our site
+  const posts = getAllPostIds();
+
+  // We generate the XML sitemap with the posts data
+  const sitemap = generateSiteMap(posts);
+
+  res.setHeader("Content-Type", "text/xml");
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
